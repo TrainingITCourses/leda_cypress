d892f4d4ae5d9d1f1e82094d512bb3b5dbceeb5f
Alberto Basalo <albertobasalo@hotmail.com>
2023-11-22 18:38:11 +0100

    feat: config cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,18 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://www.leda-mc.com/",
-    chromeWebSecurity: false,
+    baseUrl: "http://localhost:4200",
+    defaultCommandTimeout: 2000,
+    screenshotOnRunFailure: true,
+    screenshotsFolder: "cypress/screenshots",
+    trashAssetsBeforeRuns: true,
+    video: false,
+    videosFolder: "cypress/videos",
+    viewportHeight: 768,
+    viewportWidth: 1024,
+    env: {
+      apiUrl: "http://localhost:3000",
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
